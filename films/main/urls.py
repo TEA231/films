08295eef9f87caf6f81f770619f7519576742d1e
TEA231 <gabibulaevgasan888@gmail.com>
2023-10-14 20:01:50 +0300

    add_urls

--- films/main/urls.py
+++ films/main/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('register/', register),
-    path('', main)
+    path('register/', register, name='register'),
+    path('', main, name='main'),
+    path('authorization/', auth, name='autorization'),
+    path('history/', history, name='history')
 ]
 
--- films/main/views.py
+++ films/main/views.py
@@ -2,7 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def register(request):
-    return render(request, 'main/register.html')
+    return render(request, 'main/register.html', {'auth': False})
 
 def main(request):
-    return render(request, 'main/main.html')
+    return render(request, 'main/main.html', {'auth': False})
+
+def auth(request):
+    return render(request, 'main/auth.html', {'auth': False})
+
+def history(request):
+    return render(request, 'main/history.html', {'auth': False})

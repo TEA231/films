b006101c5590dfaa06931ff4aacebf40d95f7006
TEA231 <gabibulaevgasan888@gmail.com>
2023-10-16 17:19:00 +0300

    connect_postgresql

--- films/main/views.py
+++ films/main/views.py
@@ -1,14 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+context = {
+    'auth': False
+}
+
 def register(request):
-    return render(request, 'main/register.html', {'auth': False})
+    return render(request, 'main/register.html', context=context)
 
 def main(request):
-    return render(request, 'main/main.html', {'auth': False})
+    return render(request, 'main/main.html', context=context)
 
 def auth(request):
-    return render(request, 'main/auth.html', {'auth': False})
+    return render(request, 'main/auth.html', context=context)
 
 def history(request):
-    return render(request, 'main/history.html', {'auth': False})
+    return render(request, 'main/history.html', context=context)

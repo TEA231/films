1115e4b24bc661b585568375241fb5ebffe339ec
TEA231 <gabibulaevgasan888@gmail.com>
2023-10-22 19:07:11 +0300

    update_models

--- films/main/models.py
+++ films/main/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 class Films(models.Model):
+    cover = models.ImageField(upload_to="films/covers/")
+    video = models.ImageField(upload_to="films/video/")
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     category = models.CharField(max_length=255)
-    cover = models.ImageField(upload_to="films/covers/")
-    video = models.ImageField(upload_to="films/video/")
+    commets_key = models.CharField(max_length=256, null=True)
 
-    def __str__(self):
-        return self.name    
+
+class Comments(models.Model):
+    text = models.TextField(blank=True)
+    key = models.ForeignKey('Films', on_delete=models.CASCADE)
